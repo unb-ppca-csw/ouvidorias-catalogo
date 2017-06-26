b7808a9b61b8462dcaa7b9e85356e26fb3150d21
Antonio Jr <acdcjunior@gmail.com>
2017-06-25 23:48:32 -0300

    catalogo: incluir exemplo com lista de docs

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Catálogo de Ouvidorias
 
 Implementação do catálogo de ouvidorias utilizando smart contracts em Ethereum.
+
+
+# Roteiro
+
+```shell
+# Dentro do container
+
+# redeploya todos os contratos
+truffle migrate --reset
+
+# Executa os testes
+truffle test
+
+# Acessa o console
+truffle console
+
+# A partir de agora, dentro do console (note que deployed() retorna uma Promise)
+truffle(development)> Contrato.deployed().then((x) => c = x);
+
+
+```
--- catalogo-project/migrations/2_deploy_contracts.js
+++ catalogo-project/migrations/2_deploy_contracts.js
@@ -1,10 +1,13 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var MetaCoin = artifacts.require("./MetaCoin.sol");
 var ProofOfExistence1 = artifacts.require("./ProofOfExistence1.sol");
+var ProofOfExistence2 = artifacts.require("./ProofOfExistence2.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, MetaCoin);
   deployer.deploy(MetaCoin);
+
   deployer.deploy(ProofOfExistence1);
+  deployer.deploy(ProofOfExistence2);
 };

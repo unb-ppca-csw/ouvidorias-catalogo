3cbe56e40bfce97e5718475f2d4a7a8b40797376
Antonio Jr <acdcjunior@gmail.com>
2017-06-27 14:59:13 -0300

    test(catalogo): incluir teste com catalogo sendo criado no teste

--- catalogo-project/test/CatalogoOuvidorias.test.js
+++ catalogo-project/test/CatalogoOuvidorias.test.js
@@ -4,7 +4,7 @@ const CatalogoOuvidorias = artifacts.require("./CatalogoOuvidorias.sol");
 
 contract('CatalogoOuvidorias', function (accounts) {
 
-    it("Deve iniciar com dados corretos", function () {
+    it("script de deploy padrao constroi corretamente o contrato inicial", function () {
 
         let primeiraOuvidoriaAccount = accounts[0];
         CatalogoOuvidorias.deployed().then((co) => {
@@ -23,8 +23,35 @@ contract('CatalogoOuvidorias', function (accounts) {
 
                 assert.equal(primeiraOuvidoria.nome, 'CGU');
                 assert.equal(primeiraOuvidoria.enteNome, 'Uniao');
-                assert.equal(TipoEnte[primeiraOuvidoria.enteTipo.e], 'Uniao');
+                assert.equal(TipoEnte[primeiraOuvidoria.enteTipo.c[0]], 'Uniao');
                 assert.equal(primeiraOuvidoria.ouvidoriaEndpoint, 'http://cgu.gov.br/ouv');
+            });
+        });
+
+    });
+
+    it("construtor cria corretamente catalogo", function () {
+        const contractInstance = CatalogoOuvidorias.new("CGU-DF", 1, "DF", "http://cgu.gov.br/ouv-df");
+
+        let primeiraOuvidoriaAccount = accounts[0];
+        contractInstance.then((co) => {
+            let primeiraOuvidoria = {};
+            return co.getOuvidoriaNome(primeiraOuvidoriaAccount).then((nome) => {
+                primeiraOuvidoria.nome = nome;
+                return co.getOuvidoriaEnteTipo(primeiraOuvidoriaAccount);
+            }).then((enteTipo) => {
+                primeiraOuvidoria.enteTipo = enteTipo;
+                return co.getOuvidoriaEnteNome(primeiraOuvidoriaAccount);
+            }).then((enteNome) => {
+                primeiraOuvidoria.enteNome = enteNome;
+                return co.getOuvidoriaEndpoint(primeiraOuvidoriaAccount);
+            }).then((ouvidoriaEndpoint) => {
+                primeiraOuvidoria.ouvidoriaEndpoint = ouvidoriaEndpoint;
+
+                assert.equal(primeiraOuvidoria.nome, 'CGU-DF');
+                assert.equal(TipoEnte[primeiraOuvidoria.enteTipo.c[0]], 'Estado/DF');
+                assert.equal(primeiraOuvidoria.enteNome, 'DF');
+                assert.equal(primeiraOuvidoria.ouvidoriaEndpoint, 'http://cgu.gov.br/ouv-df');
             });
         });
 
